8238eb3669d37c777cd20451e316cffef20aca85
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-01-02 10:30:03 +0000

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'taro', email: 'taro@techacademy.jp', password: 'taro1234')
+User.create(name: 'hsngshj', email: 'standbyme1212@gmail.com', password: 'hsngshj1234')
+
+(1..100).each do |number|
+  Micropost.create(user_id: 1, content: 'test content ' + number.to_s)
+end
+
+(1..100).each do |number|
+  Micropost.create(user_id: 2, content: 'test content ' + number.to_s)
+end
